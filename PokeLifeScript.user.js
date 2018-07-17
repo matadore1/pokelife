ff55f2d878dd2570fb31a785bc900a2036c39a95
Jakub Mrowca <jakub.mrowca@microsoft.wsei.edu.pl>
2018-07-17 15:25:30 +0200

    change version

--- PokeLifeScript.user.js
+++ PokeLifeScript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PokeLifeScript
 // @namespace    http://tampermonkey.net/
-// @version      1.6.13
+// @version      1.6.14
 // @downloadURL  https://github.com/krozum/pokelife/raw/master/PokeLifeScript.user.js
 // @updateURL    https://github.com/krozum/pokelife/raw/master/PokeLifeScript.user.js
 // @description  Auto Attack Script
